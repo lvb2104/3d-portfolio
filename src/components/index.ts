1abfbc3558f8d6867799ef8f995accd938a0dc5f
Bao Le <levanbao2105@gmail.com>
2025-04-23 11:11:54 +0700

    feat: second commit

---
+++ src/components/index.ts
@@ -0,0 +1,24 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import About from './About';
+import Experience from './Experience';
+import Feedbacks from './Feedbacks';
+import Contact from './Contact';
+import Navbar from './Navbar';
+import Works from './Works';
+import Tech from './Tech';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
